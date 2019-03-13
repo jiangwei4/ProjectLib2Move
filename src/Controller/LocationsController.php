cc0a9301e190c77374ce262e3b97bee07af4f4bb
Reggis94 <regisboamah@gmail.com>
2019-03-13 12:41:26 +0100

    Show vehicules from offrelocation ok

--- src/Controller/LocationsController.php
+++ src/Controller/LocationsController.php
@@ -12,6 +12,7 @@ use App\Repository\GammeRepository;
 use App\Controller\OffreLocationsUserController;
 use App\Repository\OffreLocationsUserRepository;
 use App\Repository\VehiculeRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class LocationsController extends AbstractController
 {
@@ -45,18 +46,10 @@ class LocationsController extends AbstractController
         //return $this->redirectToRoute('locations');
     }
     /**
-     * @Route("/user/showLocation/{idVehicule}", name="location_show")
+     * @Route("/user/rent_vehicule/{idVehicule}", name="user_vehicule_to_rent", methods={"GET", "POST"})
      */
-    public function showLocation( EntityManagerInterface $entityManager,VehiculeRepository $VehiculeRepository)
-    {
-        //on affecte une offre a l'utilisateur 
-        $voiture = $VehiculeRepository->findAll();
-        $this->addFlash('notice', 'offre de location choisi');
-        //return $this->redirectToRoute('user/offreLocation');
-        return $this->render('locations/ListeVoiture.html.twig', [
-            'controller_name' => 'LocationsController',
-            'voiture'=>$voiture,
-        ]);
-        //return $this->redirectToRoute('locations');
+    public function userVehiculeToRent(Request $request, EntityManagerInterface $entityManager,VehiculeRepository $VehiculeRepository, $idVehicule)
+    {        
+        return $this->render('locations/user-vehicule-to-rent.html.twig', []);
     }
 }
